ba531dadaf582b4ae0c9d188eccebfde29222787
crowcasso <crowcasso@gmail.com>
2017-10-23 09:23:40 -0400

    10/20

--- Multitouch/GameScene.swift
+++ Multitouch/GameScene.swift
@@ -7,82 +7,111 @@
 //
 
 import SpriteKit
-import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    // used to build all of the other circles
+    private var circleNode : SKShapeNode?
     
+    // associative array tying UITouches to the circle on the screen
+    var touchNodes = [UITouch:SKShapeNode]()
+    
+    // 5 colors for touch points (iPhone only allows 5 touches)
+    let colors = [UIColor.red, UIColor.blue, UIColor.green, UIColor.yellow, UIColor.orange]
+    
+    // called once
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        // define the width of the circle (based on the screen size)
+        let w = (self.size.width + self.size.height) * 0.12
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        // create the circle
+        circleNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w / 2.0)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+        // set some properties on the circle
+        circleNode?.lineWidth = 15.0
+        circleNode?.isAntialiased = true
+    }
+    
+    func createCircleForTouch(touch : UITouch) {
+        
+        // copy a circle, set its position, color, and add it to the scene
+        if let n = self.circleNode?.copy() as! SKShapeNode? {
+            n.position = touch.location(in: self)
+            n.strokeColor = pickFirstColorNotInUse()
+            self.addChild(n)
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+            // associate the touch with the circle
+            touchNodes[touch] = n
         }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        
+        // loop over all touches, create a circle for that touch
+        for touch in touches {
+            createCircleForTouch(touch: touch)
         }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        
+        for touch in touches {
+            // get the circle associated with the touch
+            if let circle = touchNodes[touch] {
+                // move the circle to the new location.
+                let newLocation = touch.location(in: self)
+                circle.position = newLocation
+            }
+        }
+        
+    }
+    
+    func removeCircleForTouch (touch : UITouch ) {
+        
+        // if we can find the touch, remove the circle
+        if let circle = touchNodes[touch] {
+            circle.removeFromParent()               // remove from scene
+            touchNodes.removeValue(forKey: touch)   // remove from array
+        }
+        
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        
+        for touch in touches {
+            removeCircleForTouch(touch: touch)
+        }
+        
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        
+        for touch in touches {
+            removeCircleForTouch(touch: touch)
+        }
+
     }
     
-    
+    // choose a color currently not in use
+    func pickFirstColorNotInUse() -> UIColor {
+        for color in colors {
+            var colorInUse = false
+            for touch in touchNodes {
+                if touch.value.strokeColor == color {
+                    colorInUse = true
+                }
+            }
+            if !colorInUse {
+                return color
+            }
+        }
+        
+        return UIColor.brown
+    }
+
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
     }
